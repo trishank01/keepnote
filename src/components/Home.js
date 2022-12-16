4049445f1ebc1ea392b158ce718f4b27f1a530c6
tk <trishank01@gmail.com>
2022-12-16 23:54:12 +0530

    Drawer add

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SwiperDrawer from './SwiperDrawer'
+
+
+const Home = () => {
+  return (
+    <div>
+        <SwiperDrawer/>
+    </div>
+  )
+}
+
+export default Home
